fix sscan gradient

fla/ops/torch/sscan.py
@@ -38,5 +38,5 @@ def selective_scan(v, delta, A, B, C, D, initial_state=None):
     y = torch.stack(ys, dim=1)  # shape (b, l, d_v)
     
     y = y + v * D[None, None, :]
-    final_state = s
+    final_state = s.transpose(-1, -2) # B x d_k x d_v
     return y, final_state

fla/ops/triton/sscan/block_parallel/intra_chunk_scan.py
@@ -1,10 +1,11 @@
 import torch
 import triton 
 import triton.language as tl
+from einops import rearrange, einsum
 
 @triton.jit
 def _fwd_recurrence(
-    x, C, A_bar, B_bar, query,
+    x, H, A_bar, B_bar, query,
     y, S,
     CHUNK_SIZE: tl.constexpr, 
     D_MODEL_K: tl.constexpr, D_MODEL_V: tl.constexpr,
@@ -25,8 +26,8 @@ def _fwd_recurrence(
 
     y = y + offset_bn * CHUNK_SIZE * D_MODEL_V + offset_v * BLOCK_MODEL + tl.arange(0, BLOCK_MODEL)
 
-    C = C + offset_bn * D_MODEL_K * D_MODEL_V + offset_v * BLOCK_MODEL + tl.arange(0, D_MODEL_K)[:, None] * D_MODEL_V + tl.arange(0, BLOCK_MODEL)[None, :]
-    acc = tl.load(C)
+    H = H + offset_bn * D_MODEL_K * D_MODEL_V + offset_v * BLOCK_MODEL + tl.arange(0, D_MODEL_K)[:, None] * D_MODEL_V + tl.arange(0, BLOCK_MODEL)[None, :]
+    acc = tl.load(H)
 
     for _ in range(CHUNK_SIZE):
         x_i = tl.load(x)
@@ -49,8 +50,9 @@ def _fwd_recurrence(
 
 @triton.jit
 def _bwd_recurrence(
-    x, C, A_bar, B_bar, query, S,
-    dy, dx, dC, dA_bar, dB_bar, dquery,
+    x, H, A_bar, B_bar, query, S,
+    dy, 
+    dx, dH, dA_bar, dB_bar, dquery,
     CHUNK_SIZE: tl.constexpr, 
     D_MODEL_K: tl.constexpr, D_MODEL_V: tl.constexpr,
     BLOCK_MODEL: tl.constexpr, NUM_V_BLOCKS: tl.constexpr
@@ -68,81 +70,93 @@ def _bwd_recurrence(
     dB_bar = dB_bar + offset_bh * CHUNK_SIZE * D_MODEL_K * D_MODEL_V + offset_v * BLOCK_MODEL + tl.arange(0, D_MODEL_K)[:, None] * D_MODEL_V + tl.arange(0, BLOCK_MODEL)[None, :] + (CHUNK_SIZE - 1) * D_MODEL_K * D_MODEL_V
 
     # current state
-    S1 = S + offset_bh * CHUNK_SIZE * D_MODEL_K * D_MODEL_V + offset_v * BLOCK_MODEL + tl.arange(0, D_MODEL_K)[:, None] * D_MODEL_V + tl.arange(0, BLOCK_MODEL)[None, :] + (CHUNK_SIZE - 1) * D_MODEL_K * D_MODEL_V
-    # previous state
-    S2 = S + offset_bh * CHUNK_SIZE * D_MODEL_K * D_MODEL_V + offset_v * BLOCK_MODEL + tl.arange(0, D_MODEL_K)[:, None] * D_MODEL_V + tl.arange(0, BLOCK_MODEL)[None, :] + (CHUNK_SIZE - 2) * D_MODEL_K * D_MODEL_V
+    S = S + offset_bh * CHUNK_SIZE * D_MODEL_K * D_MODEL_V + offset_v * BLOCK_MODEL + tl.arange(0, D_MODEL_K)[:, None] * D_MODEL_V + tl.arange(0, BLOCK_MODEL)[None, :] + (CHUNK_SIZE - 1) * D_MODEL_K * D_MODEL_V
 
-    # initial state
-    C = C + offset_bh * D_MODEL_K * D_MODEL_V + offset_v * BLOCK_MODEL + tl.arange(0, D_MODEL_K)[:, None] * D_MODEL_V + tl.arange(0, BLOCK_MODEL)[None, :]
-    dC = dC + offset_bh * D_MODEL_K * D_MODEL_V + offset_v * BLOCK_MODEL + tl.arange(0, D_MODEL_K)[:, None] * D_MODEL_V + tl.arange(0, BLOCK_MODEL)[None, :]
+    H = H + offset_bh * D_MODEL_K * D_MODEL_V + offset_v * BLOCK_MODEL + tl.arange(0, D_MODEL_K)[:, None] * D_MODEL_V + tl.arange(0, BLOCK_MODEL)[None, :]
+    dH = dH + offset_bh * D_MODEL_K * D_MODEL_V + offset_v * BLOCK_MODEL + tl.arange(0, D_MODEL_K)[:, None] * D_MODEL_V + tl.arange(0, BLOCK_MODEL)[None, :]
 
     query = query + offset_bh * CHUNK_SIZE * D_MODEL_K + tl.arange(0, D_MODEL_K) + (CHUNK_SIZE - 1) * D_MODEL_K
     dquery = dquery + offset_bh * CHUNK_SIZE * NUM_V_BLOCKS * D_MODEL_K + (CHUNK_SIZE - 1) * NUM_V_BLOCKS * D_MODEL_K + offset_v * D_MODEL_K + tl.arange(0, D_MODEL_K)
 
     dy = dy + offset_bh * CHUNK_SIZE * D_MODEL_V + offset_v * BLOCK_MODEL + tl.arange(0, BLOCK_MODEL) + (CHUNK_SIZE - 1) * D_MODEL_V
 
-    Dacc = tl.zeros([D_MODEL_K, BLOCK_MODEL], dtype=tl.float32)
+    D_acc = tl.zeros([D_MODEL_K, BLOCK_MODEL], dtype=tl.float32)
+
+    # last item
+    S_i = tl.load(S)
+    dy_i = tl.load(dy)
+    query_i = tl.load(query)
+
+    dquery_i = tl.sum(dy_i[None, :] * S_i, axis=1)
+    tl.store(dquery, dquery_i.to(x.dtype.element_ty))
+
+    D_acc += query_i[:, None] * dy_i[None, :]
+
+    x_i = tl.load(x)
+    B_bar_i = tl.load(B_bar)
+    dB_bar_i = x_i[None, :] * D_acc
+    dx_i = tl.sum(B_bar_i * D_acc, axis=0)
+    tl.store(dB_bar, dB_bar_i.to(x.dtype.element_ty))
+    tl.store(dx, dx_i.to(x.dtype.element_ty))
+
+    x -= D_MODEL_V
+    dx -= D_MODEL_V
+    dy -= D_MODEL_V
+    query -= D_MODEL_K
+    dquery -= D_MODEL_K * NUM_V_BLOCKS
+    B_bar -= D_MODEL_K * D_MODEL_V
+    dB_bar -= D_MODEL_K * D_MODEL_V
+    S -= D_MODEL_K * D_MODEL_V
+
+
+    # middle items
     for _ in range(CHUNK_SIZE - 1):
+        S_i = tl.load(S)
         dy_i = tl.load(dy)
         query_i = tl.load(query)
-        S1_i = tl.load(S1)
-        S2_i = tl.load(S2)
-        Dacc += query_i[:, None] * dy_i[None, :]
-        dquery_i = tl.sum(dy_i[None, :] * S1_i, axis=1)
+
+        dquery_i = tl.sum(dy_i[None, :] * S_i, axis=1)
         tl.store(dquery, dquery_i.to(x.dtype.element_ty))
+    
+        dA_bar_i = S_i * D_acc
+        tl.store(dA_bar, dA_bar_i.to(x.dtype.element_ty))
+        dA_bar -= D_MODEL_K * D_MODEL_V
+
+        # update \nabla_H L
+        A_bar_i = tl.load(A_bar)
+        A_bar -= D_MODEL_K * D_MODEL_V
+        D_acc = D_acc * A_bar_i + query_i[:, None] * dy_i[None, :]
 
         x_i = tl.load(x)
-        A_bar_i = tl.load(A_bar)
         B_bar_i = tl.load(B_bar)
-
-        dA_bar_i = S2_i * Dacc
-        dB_bar_i = x_i[None, :] * Dacc
-        dx_i = tl.sum(B_bar_i * Dacc, axis=0)
-        tl.store(dA_bar, dA_bar_i.to(x.dtype.element_ty))
+        dB_bar_i = x_i[None, :] * D_acc
+        dx_i = tl.sum(B_bar_i * D_acc, axis=0)
         tl.store(dB_bar, dB_bar_i.to(x.dtype.element_ty))
         tl.store(dx, dx_i.to(x.dtype.element_ty))
-        
-        Dacc = A_bar_i * Dacc
 
         x -= D_MODEL_V
         dx -= D_MODEL_V
+        dy -= D_MODEL_V
         query -= D_MODEL_K
         dquery -= D_MODEL_K * NUM_V_BLOCKS
-        A_bar -= D_MODEL_K * D_MODEL_V
         B_bar -= D_MODEL_K * D_MODEL_V
-        dA_bar -= D_MODEL_K * D_MODEL_V
         dB_bar -= D_MODEL_K * D_MODEL_V
-        S1 -= D_MODEL_K * D_MODEL_V
-        S2 -= D_MODEL_K * D_MODEL_V
-    
-    # handle first element
-    dy_i = tl.load(dy)
-    query_i = tl.load(query)
-    S1_i = tl.load(S1)
-    S2_i = tl.load(C) # inital state
-    Dacc += query_i[:, None] * dy_i[None, :]
-    dquery_i = tl.sum(dy_i[None, :] * S1_i, axis=1)
-    tl.store(dquery, dquery_i.to(x.dtype.element_ty))
+        S -= D_MODEL_K * D_MODEL_V
 
-    x_i = tl.load(x)
+    # the first telement
+    S_i = tl.load(H)
+    dA_bar_i = S_i * D_acc
     A_bar_i = tl.load(A_bar)
-    B_bar_i = tl.load(B_bar)
-
-    dA_bar_i = S2_i * Dacc
-    dB_bar_i = x_i[None, :] * Dacc
-    dx_i = tl.sum(B_bar_i * Dacc, axis=0)
     tl.store(dA_bar, dA_bar_i.to(x.dtype.element_ty))
-    tl.store(dB_bar, dB_bar_i.to(x.dtype.element_ty))
-    tl.store(dx, dx_i.to(x.dtype.element_ty))
+    D_acc = A_bar_i * D_acc
+    tl.store(dH, D_acc.to(x.dtype.element_ty))
     
-    Dacc = A_bar_i * Dacc
-    tl.store(dC, Dacc.to(x.dtype.element_ty))
     
 class IntraChunkScan(torch.autograd.Function):
     @staticmethod
-    def forward(ctx, x, C, A_bar, B_bar, query):
+    def forward(ctx, x, H, A_bar, B_bar, query):
         x = x.contiguous()
-        C = C.contiguous()
+        H = H.contiguous()
         A_bar = A_bar.contiguous()
         B_bar = B_bar.contiguous()
         query = query.contiguous()
@@ -160,27 +174,27 @@ class IntraChunkScan(torch.autograd.Function):
         ctx.BLOCK_MODEL = BLOCK_MODEL
 
         _fwd_recurrence[grid](
-            x, C, A_bar, B_bar, query,
+            x, H, A_bar, B_bar, query,
             y, S,
             CHUNK_SIZE=L,  
             D_MODEL_K=D_k, D_MODEL_V=D_v,
             BLOCK_MODEL=BLOCK_MODEL
         )
         
-        ctx.save_for_backward(x, C, A_bar, B_bar, query, S) 
+        ctx.save_for_backward(x, H, A_bar, B_bar, query, S) 
         return y 
 
     @staticmethod
     def backward(ctx, dy):
         dy = dy.contiguous()
 
-        x, C, A_bar, B_bar, query, S = ctx.saved_tensors 
+        x, H, A_bar, B_bar, query, S = ctx.saved_tensors 
         B, N, L, D_k, D_v = S.shape 
 
         grid = ctx.grid 
         BLOCK_MODEL = ctx.BLOCK_MODEL 
         dx = torch.empty_like(x)
-        dC = torch.empty_like(C)
+        dH = torch.empty_like(H)
         dA_bar = torch.empty_like(A_bar)
         dB_bar = torch.empty_like(B_bar)
 
@@ -189,8 +203,9 @@ class IntraChunkScan(torch.autograd.Function):
         dquery = torch.empty(B, N, L, num_v_blocks, D_k, device=x.device, dtype=x.dtype)
 
         _bwd_recurrence[grid](
-            x, C, A_bar, B_bar, query, S, 
-            dy, dx, dC, dA_bar, dB_bar, dquery,
+            x, H, A_bar, B_bar, query, S, 
+            dy, 
+            dx, dH, dA_bar, dB_bar, dquery,
             CHUNK_SIZE = L,
             D_MODEL_K = D_k,
             D_MODEL_V = D_v, 
@@ -198,4 +213,54 @@ class IntraChunkScan(torch.autograd.Function):
             NUM_V_BLOCKS = num_v_blocks
         )
         dquery = dquery.sum(dim=3)
-        return dx, dC, dA_bar, dB_bar, dquery
+        return dx, dH, dA_bar, dB_bar, dquery
+
+def naive_chunk_scan(v, H, A_bar, B_bar, C):
+    bs, num_chunk, chunk_size, d_k, d_v = A_bar.shape
+
+    s = H
+    res = []
+    for idx in range(chunk_size):
+        s = A_bar[:, :, idx] * s + B_bar[:, :, idx] * v[:, :, idx, None, :]
+        o = einsum(s, C[:, :, idx],"b n dk dv, b n dk -> b n dv") 
+        res.append(o)
+    return torch.stack(res, dim=2)
+
+
+
+if __name__ == "__main__":
+    d_k = 16
+    bs, num_chunk, chunk_size, d_v = 4, 8, 16, 128
+    v = torch.randn((bs, num_chunk, chunk_size, d_v), dtype=torch.float32, device='cuda').requires_grad_(True)
+    A = torch.randn((bs, num_chunk, chunk_size, d_k, d_v), dtype=torch.float32, device='cuda').requires_grad_(True)
+    log_A_bar = - torch.exp(A)
+    A_bar = torch.exp(log_A_bar)
+    B_bar = torch.randn((bs, num_chunk, chunk_size, d_k, d_v), dtype=torch.float32, device='cuda').requires_grad_(True)
+    C = torch.randn((bs, num_chunk, chunk_size, d_k), dtype=torch.float32, device='cuda').requires_grad_(True)
+    H = torch.randn((bs, num_chunk, d_k, d_v), dtype=torch.float32, device='cuda').requires_grad_(True)
+
+    do = torch.randn_like(v)
+
+    ref = naive_chunk_scan(v, H, A_bar, B_bar, C)
+    ref.backward(do, retain_graph=True)
+    ref_dA, A.grad = A.grad.clone(), None
+    ref_dB, B_bar.grad = B_bar.grad.clone(), None
+    ref_dv, v.grad = v.grad.clone(), None
+    ref_C, C.grad = C.grad.clone(), None
+    ref_H, H.grad = H.grad.clone(), None
+
+    tri = IntraChunkScan.apply(v, H, A_bar, B_bar, C)
+    tri.backward(do)
+    tri_dA, A.grad = A.grad.clone(), None
+    tri_dB, B_bar.grad = B_bar.grad.clone(), None
+    tri_dv, v.grad = v.grad.clone(), None
+    tri_C, C.grad = C.grad.clone(), None
+    tri_H, H.grad = H.grad.clone(), None
+
+    breakpoint()
+    assert ref.allclose(tri, 0, 1e1)
+    assert ref_dA.allclose(tri_dA, 0, 1e1)
+    assert ref_dB.allclose(tri_dB, 0, 1e-0)
+    assert ref_dv.allclose(tri_dv, 0, 1e-0)
+    assert ref_C.allclose(tri_C, 0, 1e-0)
+    assert ref_H.allclose(tri_H, 0, 1e-0)

fla/ops/triton/sscan/block_parallel/sscan_block_parallel.py
@@ -25,8 +25,8 @@ def selective_scan_block_parallel(v, delta, A, B, C, D, initial_state=None):
 
     chunk_state = IntraChunkReduce.apply(v, A_bar, B_bar)
     chunk_gate = log_A_bar.sum(dim=2).exp()
-    chunk_state = InterChunkScan.apply(chunk_state, chunk_gate)
+    chunk_state, final_state = InterChunkScan.apply(chunk_state, chunk_gate, initial_state)
     y = IntraChunkScan.apply(v, chunk_state, A_bar, B_bar, C)
     y = y + v * D[None, None, :]
     y = rearrange(y, "b n c dv -> b (n c) dv")
-    return y, None
+    return y, final_state

tests/ops/test_sscan.py
@@ -21,11 +21,11 @@ def test_sscan(dtype, d_v, L, d_k=16, bs=4):
     D = torch.randn(d_v, dtype=dtype, device='cuda').requires_grad_(True)
     delta_prime = torch.randn((bs, L, d_v), dtype=dtype, device='cuda').requires_grad_(True)
     delta = F.softplus(delta_prime)
-    # initial_hidden_state = torch.randn((bs, d_v, d_k), dtype=dtype, device='cuda').requires_grad_(True)
-    initial_hidden_state = None
+    initial_state = torch.randn((bs, d_v, d_k), dtype=dtype, device='cuda').requires_grad_(False)
+    # initial_state = None
 
     do = torch.randn_like(v) 
-    ref, next_hidden_state = naive_selective_scan(v, delta, A, B, C, D, initial_hidden_state)
+    ref, ref_final_state = naive_selective_scan(v, delta, A, B, C, D, initial_state)
     ref.backward(do, retain_graph=True)
     ref_dv, v.grad = v.grad.clone(), None
     ref_dA_prime, A_prime.grad = A_prime.grad.clone(), None
@@ -33,9 +33,9 @@ def test_sscan(dtype, d_v, L, d_k=16, bs=4):
     ref_dC, C.grad = C.grad.clone(), None
     ref_dD, D.grad = D.grad.clone(), None
     ref_ddelta_prime, delta_prime.grad = delta_prime.grad.clone(), None
-    # ref_initial_hidden_state, initial_hidden_state.grad = initial_hidden_state.grad.clone(), None
 
-    tri, next_hidden_state = selective_scan_block_parallel(v, delta, A, B, C, D, initial_hidden_state)
+    initial_state = initial_state.transpose(1, 2).contiguous() # b * dk * dv
+    tri, tri_final_state = selective_scan_block_parallel(v, delta, A, B, C, D, initial_state)
     tri.backward(do, retain_graph=True)
     tri_dv, v.grad = v.grad.clone(), None
     tri_dA_prime, A_prime.grad = A_prime.grad.clone(), None
@@ -43,17 +43,17 @@ def test_sscan(dtype, d_v, L, d_k=16, bs=4):
     tri_dC, C.grad = C.grad.clone(), None
     tri_dD, D.grad = D.grad.clone(), None
     tri_ddelta_prime, delta_prime.grad = delta_prime.grad.clone(), None
-    # tri_initial_hidden_state, initial_hidden_state.grad = initial_hidden_state.grad.clone(), None
 
-    breakpoint()
-    assert ref.allclose(tri, 0, 1e1)
-    assert ref_dv.allclose(tri_dv, 0, 1e1)
-    assert ref_dA_prime.allclose(tri_dA_prime, 0, 1e1)
-    assert ref_dB.allclose(tri_dB, 0, 1e1)
-    assert ref_dC.allclose(tri_dC, 0, 1e1)
-    assert ref_dD.allclose(tri_dD, 0, 1e1)
+    # breakpoint()
+    assert ref.allclose(tri, 0, 1e-1)
+    assert ref_final_state.allclose(tri_final_state, 0, 1e-1)
+    assert ref_dv.allclose(tri_dv, 0, 1e-1)
+    assert ref_dA_prime.allclose(tri_dA_prime, 0, 1e-1)
+    assert ref_dB.allclose(tri_dB, 0, 1e-1)
+    assert ref_dC.allclose(tri_dC, 0, 1e-1)
+    assert ref_dD.allclose(tri_dD, 0, 1e-1)
     assert ref_ddelta_prime.allclose(tri_ddelta_prime, 0, 1e-1)
-    # assert ref_initial_hidden_state.allclose(tri_initial_hidden_state, 0, 1e-1)
+    assert ref_final_state.allclose(tri_final_state, 0, 1e-1)
 
     print('Done!')
 